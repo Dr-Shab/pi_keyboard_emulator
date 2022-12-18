e39c30049cd936bf8bdf84a4f2f450fe1c9da9dd
Dr-Shab <drishabani@hotmail.com>
2022-12-18 01:09:28 +0100

    wrapped everything in a beautiful function and added the sys.argv argument which enables it to add simple argument when using from commandline

--- translator.py
+++ translator.py
@@ -1,58 +1,67 @@
+import sys
+
 specials = "+\"*ç%&/()=?`|@#¼½¬|¢]}´~[]{}!,;.:-_<\\>"
 
 
-def letter_to_key(word):
+def letter_to_key(word, file):
     for letter in word:
         if letter.isnumeric():
-            f.write(f"    write_report(_.number[\"{letter}\"])\n")
-            f.write("    write_report(_.operation[\"release\"])\n")
-            f.write("    sleep(0.1)\n\n")
+            file.write(f"    write_report(_.number[\"{letter}\"])\n")
+            file.write("    write_report(_.operation[\"release\"])\n")
+            file.write("    sleep(0.1)\n\n")
 
         elif letter in specials:
-            f.write(f"    write_report(_.special_key[\"{letter}\"])\n")
-            f.write("    sleep(0.1)\n\n")  # when shift key is pressed sleep needs to be here so the terminal can react...
-            f.write("    write_report(_.operation[\"release\"])\n")
+            file.write(f"    write_report(_.special_key[\"{letter}\"])\n")
+            file.write("    sleep(0.1)\n\n")  # when shift key is pressed sleep needs to be here so the terminal can react...
+            file.write("    write_report(_.operation[\"release\"])\n")
 
         elif letter.islower():
-            f.write(f"    write_report(_.lowercase[\"{letter}\"])\n")
-            f.write("    write_report(_.operation[\"release\"])\n")
-            f.write("    sleep(0.1)\n\n")
+            file.write(f"    write_report(_.lowercase[\"{letter}\"])\n")
+            file.write("    write_report(_.operation[\"release\"])\n")
+            file.write("    sleep(0.1)\n\n")
 
         elif letter.isupper():
-            f.write(f"    write_report(_.uppercase[\"{letter}\"])\n")
-            f.write("    sleep(0.1)\n\n")  # when shift key is pressed sleep needs to be here so the terminal can react...
-            f.write("    write_report(_.operation[\"release\"])\n")
+            file.write(f"    write_report(_.uppercase[\"{letter}\"])\n")
+            file.write("    sleep(0.1)\n\n")  # when shift key is pressed sleep needs to be here so the terminal can react...
+            file.write("    write_report(_.operation[\"release\"])\n")
 
-    f.write("    write_report(_.special_key[\"space\"])\n")
-    f.write("    write_report(_.operation[\"release\"])\n")
-    f.write("    sleep(0.1)\n\n")
+    file.write("    write_report(_.special_key[\"space\"])\n")
+    file.write("    write_report(_.operation[\"release\"])\n")
+    file.write("    sleep(0.1)\n\n")
     return "lets buckling go"
 
 
-with open('victim_script.py', 'x') as f:
-    f.write("from time import sleep\nimport keys_ch_keyboard as _\nfrom os import name as OS\n\n")
-    f.write("sleep(5)\n\nNULL_CHAR = chr(0)\n\n\n")
-    f.write("def write_report(report):\n    with open('/dev/hidg0', 'rb+') as fd:\n         fd.write(report.encode())\n\n\n")
-    f.write("if OS == 'posix':\n")
-    f.write("    write_report(_.operation[\"release\"])\n")
-    f.write("    write_report(_.operation[\"terminal_linux\"])\n")
-    f.write("    write_report(_.operation[\"release\"])\n")
-    f.write("    sleep(2)\n\n")
-    with open('raw_script_posix.txt', 'r') as rsf:
-        lines = rsf.readlines()
-        for line in lines:
-            words = line.split(" ")
-            for word in words:
-                if "\n" in word:
-                    letter_to_key(word.strip("\n"))
-                    f.write("    write_report(_.special_key[\"enter\"])\n")
-                    f.write("    write_report(_.operation[\"release\"])\n\n")
-                    f.write("    sleep(0.5)\n\n")
-                else:
-                    letter_to_key(word)
+def translate(cmd_file):
+    lang = input("Which keyboard layout is used? ch or.. ")
+    if len(lang) == 2 and lang.isalpha():
+        with open('cmd_to_strokes.py', 'x') as wf:
+            wf.write(f"from time import sleep\nimport keys_{lang}_keyboard as _\nfrom os import name as OS\n\n")
+            wf.write("sleep(5)\n\nNULL_CHAR = chr(0)\n\n\n")
+            wf.write(
+                "def write_report(report):\n    with open('/dev/hidg0', 'rb+') as fd:\n         fd.write(report.encode())\n\n\n")
+            wf.write("if OS == 'posix':\n")
+            wf.write("    write_report(_.operation[\"release\"])\n")
+            wf.write("    write_report(_.operation[\"terminal_linux\"])\n")
+            wf.write("    write_report(_.operation[\"release\"])\n")
+            wf.write("    sleep(2)\n\n")
+            with open(cmd_file, 'r') as rf:
+                lines = rf.readlines()
+                for line in lines:
+                    words = line.split(" ")
+                    for word in words:
+                        if "\n" in word:
+                            letter_to_key(word.strip("\n"), wf)
+                            wf.write("    write_report(_.special_key[\"enter\"])\n")
+                            wf.write("    write_report(_.operation[\"release\"])\n\n")
+                            wf.write("    sleep(0.5)\n\n")
+                        else:
+                            letter_to_key(word, wf)
+        print()
+        exit(0)
+    else:
+        print("are you kidding me?!")
+        exit(1)
 
 
-
-
-
-
+if __name__ == "__main__":
+    translate(str(sys.argv[1]))
